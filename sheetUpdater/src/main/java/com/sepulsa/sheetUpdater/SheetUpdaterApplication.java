b2cec132788d3f305e9107531faec6fb3b9c8f59
sepulsa <fahreza.tamara@gmail.com>
2017-02-09 15:34:27 +0700

    te

--- sheetUpdater/src/main/java/com/sepulsa/sheetUpdater/SheetUpdaterApplication.java
+++ sheetUpdater/src/main/java/com/sepulsa/sheetUpdater/SheetUpdaterApplication.java
@@ -1,15 +1,27 @@
 package com.sepulsa.sheetUpdater;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.google.api.services.sheets.v4.Sheets;
+import com.sepulsa.sheetUpdater.Service.SheetService;
 
 @SpringBootApplication
 @ComponentScan("com.sepulsa")
 public class SheetUpdaterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SheetUpdaterApplication.class, args);
+		// Build a new authorized API client service.
+		try {
+			SheetService sheetService = new SheetService();
+			Sheets service = sheetService.getSheetsService();
+			SpringApplication.run(SheetUpdaterApplication.class, args);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
 	
