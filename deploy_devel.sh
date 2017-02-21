e6e1023765200947afa1fd32db6dffdfaa97c40d
sepulsa <fahreza.tamara@gmail.com>
2017-02-21 17:39:54 +0700

    add deploy_devel.sh

---
+++ deploy_devel.sh
@@ -0,0 +1,14 @@
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+
+cd $DEVELOPMENT_APP_PATH
+git status
+git fetch origin
+git checkout $DEVELOPMENT_BRANCH
+git pull origin $DEVELOPMENT_BRANCH
+
+#STOP APPLICATION
+curl -X POST $DEVELOPMENT_HOST:$DEVELOPMENT_PORT/shutdown
+
+#START APPLICATION
+mvn spring-boot:run
